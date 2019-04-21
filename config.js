8613ae9e976e60c7e69390b90bb94b94c72d9aa6
shaikhuzma <shaikhu75@gmail.com>
2019-04-21 11:11:30 +0530

    login

---
+++ config.js
@@ -0,0 +1,10 @@
+ 
+  var config = {
+    apiKey: "AIzaSyDj7dcCGpDMIwIqTehkZ6337_eONn2OD9k",
+    authDomain: "hexception.firebaseapp.com",
+    databaseURL: "https://hexception.firebaseio.com",
+    projectId: "hexception",
+    storageBucket: "hexception.appspot.com",
+    messagingSenderId: "233242348861"
+  };
+  firebase.initializeApp(config);
